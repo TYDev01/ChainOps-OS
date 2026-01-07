11b49861bf102c634fd6003af5f4d88040199bc1
Anthony <anthonychinaemeru84@yahoo.com>
2026-01-07 19:52:12 +0100

    Add disable  agent

--- src/AgentManager.sol
+++ src/AgentManager.sol
@@ -47,4 +47,12 @@ contract AgentManager is Roles {
         agents[agent].enabled = true;
         emit AgentEnabled(agent);
     }
+
+    function disableAgent(address agent) external onlyRole(AGENT_ADMIN) {
+        if (agents[agent].metadataHash == bytes32(0)) {
+            revert Errors.NotRegistered();
+        }
+        agents[agent].enabled = false;
+        emit AgentDisabled(agent);
+    }
 }
