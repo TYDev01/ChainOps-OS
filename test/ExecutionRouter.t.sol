8dee5920515750dae8de48bf1682273fa2a864a6
Anthony <anthonychinaemeru84@yahoo.com>
2026-01-07 22:13:32 +0100

    Add ExecutionRouter constructor test

---
+++ test/ExecutionRouter.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "forge-std/Test.sol";
+import {ExecutionRouter} from "../src/ExecutionRouter.sol";
+import {ExecutionTypes} from "../src/ExecutionTypes.sol";
+
+contract ExecutionRouterTest is Test {
+    function testConstructorSetsAdmin() public {
+        address admin = address(0xA11CE);
+        ExecutionRouter router = new ExecutionRouter(admin);
+        assertTrue(router.hasRole(router.DEFAULT_ADMIN_ROLE(), admin));
+    }
+}
